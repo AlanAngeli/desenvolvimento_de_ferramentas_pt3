dd55a8c40884559f187d7b0e0f0a213e621cf2f1
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-03-28 20:14:28 -0300

    Add files via upload

---
+++ ocultador_de_arquivos.py
@@ -0,0 +1,17 @@
+"""
+
+- Biblioteca ctypes: fornece tipos de dados compatíveis com C e permite funções
+de chamada em DLLs ou bibliotecas compartilhadas
+
+"""
+
+import ctypes
+
+atributo_ocultar = 0x02
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+
+if retorno:
+    print('Arquivo foi ocultado')
+else:
+    print('O arquivo não foi ocultado')
